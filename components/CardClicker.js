2390fa9b3826f8fad63f111adc128794a0674641
Steve Geluso <stevegeluso@gmail.com>
2018-05-25 20:50:58 -0700

    adding autobind-class-methods

--- components/CardClicker.js
+++ components/CardClicker.js
@@ -1,25 +1,36 @@
 import React, {Component, Fragment} from 'react';
 import card01 from '../assets/raw-photos/01.jpg';
+import deck from './CardList';
 
 class CardClicker extends Component {
-  currentCard = 0;
+  state = {currentCard: 0};
 
   dec = () => {
-    this.currentCard--;
+    this.setState({currentCard: this.state.currentCard - 1});
   }
 
   inc = () => {
-    console.log(this.currentCard);
-    this.currentCard++;
+    this.setState({currentCard: this.state.currentCard + 1});
+  }
+
+  card = () => {
+    let card = this.state.currentCard;
+    if (this.state.currentCard < 10) {
+      card = '0' + this.state.currentCard;
+    }
+    card = 'card' + card;
+    let img = deck[card];
+    console.log('img', img);
+    return img;
   }
 
   render() {
     return <Fragment>
       <p>
-        {card01} {' '} {this.currentCard}
+        {this.card()} {' '} {this.state.currentCard}
       </p>
       <div>
-        <img src={card01} />
+        <img src={this.card()} />
       </div>
       <div className="pagination-controls">
         <button onClick={this.dec}>prev</button>
--- components/CardList.js
+++ components/CardList.js
@@ -1,60 +1,115 @@
+import card00 from '../assets/raw-photos/back.jpg';
+import card01 from '../assets/raw-photos/01.jpg';
+import card02 from '../assets/raw-photos/02.jpg';
+import card03 from '../assets/raw-photos/03.jpg';
+import card04 from '../assets/raw-photos/04.jpg';
+import card05 from '../assets/raw-photos/05.jpg';
+import card06 from '../assets/raw-photos/06.jpg';
+import card07 from '../assets/raw-photos/07.jpg';
+import card08 from '../assets/raw-photos/08.jpg';
+import card09 from '../assets/raw-photos/09.jpg';
+import card10 from '../assets/raw-photos/10.jpg';
+import card11 from '../assets/raw-photos/11.jpg';
+import card12 from '../assets/raw-photos/12.jpg';
+import card13 from '../assets/raw-photos/13.jpg';
+import card14 from '../assets/raw-photos/14.jpg';
+import card15 from '../assets/raw-photos/15.jpg';
+import card16 from '../assets/raw-photos/16.jpg';
+import card17 from '../assets/raw-photos/17.jpg';
+import card18 from '../assets/raw-photos/18.jpg';
+import card19 from '../assets/raw-photos/19.jpg';
+import card20 from '../assets/raw-photos/20.jpg';
+import card21 from '../assets/raw-photos/21.jpg';
+import card22 from '../assets/raw-photos/22.jpg';
+import card23 from '../assets/raw-photos/23.jpg';
+import card24 from '../assets/raw-photos/24.jpg';
+import card25 from '../assets/raw-photos/25.jpg';
+import card26 from '../assets/raw-photos/26.jpg';
+import card27 from '../assets/raw-photos/27.jpg';
+import card28 from '../assets/raw-photos/28.jpg';
+import card29 from '../assets/raw-photos/29.jpg';
+import card30 from '../assets/raw-photos/30.jpg';
+import card31 from '../assets/raw-photos/31.jpg';
+import card32 from '../assets/raw-photos/32.jpg';
+import card33 from '../assets/raw-photos/33.jpg';
+import card34 from '../assets/raw-photos/34.jpg';
+import card35 from '../assets/raw-photos/35.jpg';
+import card36 from '../assets/raw-photos/36.jpg';
+import card37 from '../assets/raw-photos/37.jpg';
+import card38 from '../assets/raw-photos/38.jpg';
+import card39 from '../assets/raw-photos/39.jpg';
+import card40 from '../assets/raw-photos/40.jpg';
+import card41 from '../assets/raw-photos/41.jpg';
+import card42 from '../assets/raw-photos/42.jpg';
+import card43 from '../assets/raw-photos/43.jpg';
+import card44 from '../assets/raw-photos/44.jpg';
+import card45 from '../assets/raw-photos/45.jpg';
+import card46 from '../assets/raw-photos/46.jpg';
+import card47 from '../assets/raw-photos/47.jpg';
+import card48 from '../assets/raw-photos/48.jpg';
+import card49 from '../assets/raw-photos/49.jpg';
+import card50 from '../assets/raw-photos/50.jpg';
+import card51 from '../assets/raw-photos/51.jpg';
+import card52 from '../assets/raw-photos/52.jpg';
+import card53 from '../assets/raw-photos/53.jpg';
+import card54 from '../assets/raw-photos/54.jpg';
+import card55 from '../assets/raw-photos/55.jpg';
+
 export default {
-  cards: [
-    '01.jpg',
-    '02.jpg',
-    '03.jpg',
-    '04.jpg',
-    '05.jpg',
-    '06.jpg',
-    '07.jpg',
-    '08.jpg',
-    '09.jpg',
-    '10.jpg',
-    '11.jpg',
-    '12.jpg',
-    '13.jpg',
-    '14.jpg',
-    '15.jpg',
-    '16.jpg',
-    '17.jpg',
-    '18.jpg',
-    '19.jpg',
-    '20.jpg',
-    '21.jpg',
-    '22.jpg',
-    '23.jpg',
-    '24.jpg',
-    '25.jpg',
-    '26.jpg',
-    '27.jpg',
-    '28.jpg',
-    '29.jpg',
-    '30.jpg',
-    '31.jpg',
-    '32.jpg',
-    '33.jpg',
-    '34.jpg',
-    '35.jpg',
-    '36.jpg',
-    '37.jpg',
-    '38.jpg',
-    '39.jpg',
-    '40.jpg',
-    '41.jpg',
-    '42.jpg',
-    '43.jpg',
-    '44.jpg',
-    '45.jpg',
-    '46.jpg',
-    '47.jpg',
-    '48.jpg',
-    '49.jpg',
-    '50.jpg',
-    '51.jpg',
-    '52.jpg',
-    '53.jpg',
-    '54.jpg',
-    '55.jpg',
-    'back.jpg',
-  ]
-}
+  card00,
+  card01,
+  card02,
+  card03,
+  card04,
+  card05,
+  card06,
+  card07,
+  card08,
+  card09,
+  card10,
+  card11,
+  card12,
+  card13,
+  card14,
+  card15,
+  card16,
+  card17,
+  card18,
+  card19,
+  card20,
+  card21,
+  card22,
+  card23,
+  card24,
+  card25,
+  card26,
+  card27,
+  card28,
+  card29,
+  card30,
+  card31,
+  card32,
+  card33,
+  card34,
+  card35,
+  card36,
+  card37,
+  card38,
+  card39,
+  card40,
+  card41,
+  card42,
+  card43,
+  card44,
+  card45,
+  card46,
+  card47,
+  card48,
+  card49,
+  card50,
+  card51,
+  card52,
+  card53,
+  card54,
+  card55,
+};
